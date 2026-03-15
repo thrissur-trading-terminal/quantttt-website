8fd3f59995d333ab4dda97e70720970cd5836a2d
Arjun <arjun@quantttt.com>
2026-03-15 17:06:08 +0530

    add GA4 analytics tracking to all pages

---
+++ google_analytics.js
@@ -0,0 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+
+var s1 = document.createElement('script');
+s1.async = true;
+s1.src = 'https://www.googletagmanager.com/gtag/js?id=G-3KH3766LGT';
+document.head.appendChild(s1);
+
+gtag('js', new Date());
+gtag('config', 'G-3KH3766LGT');
